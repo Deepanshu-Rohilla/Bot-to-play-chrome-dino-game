63fa324f62c65e2b19459bad3f8febfc091d5f2d
Deepanshu-Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-05-03 20:47:30 +0530

    Works in most cases
    In some cases however, it fails to jump

--- AUTO-DINO/main.py
+++ AUTO-DINO/main.py
@@ -1,20 +1,28 @@
 import pyautogui
 from PIL import Image, ImageGrab
-from numpy import asarray
+#from numpy import asarray
 import time
 def press(key):
     pyautogui.keyDown(key)
 
-def takeScreenshot():
-    image = ImageGrab.grab().convert('L')
-    return image
+def danger(data):
+    for i in range(250,325):
+        for j in range(400,500):
+            if(data[i,j]>60):
+                return True
+    return False
+
 
 if __name__ == "__main__":
-    time.sleep(1)
-    image = takeScreenshot()
-    data=image.load()
-    print(asarray(image))
-    for i in range(34,45):
-        for j in range(45,67):
-            data[i, j] = 0
-    image.show()
+    time.sleep(3)
+    press("up")
+    while(True):
+        image = ImageGrab.grab().convert('L')
+        data=image.load()
+        if danger(data):
+            press("up")
+    # print(asarray(image))
+    # for i in range(250,325):
+    #     for j in range(400,525):
+    #         data[i, j] = 0
+    # image.show()
